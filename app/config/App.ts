7ce94ab00baf74355b0dd49833ac7c3e2ba2b169
Jason Appiatu <appiatujason@gmail.com>
2023-03-01 16:19:34 +0000

    Google login/signup working

--- app/config/App.ts
+++ app/config/App.ts
@@ -1,75 +1,98 @@
 import { json } from "body-parser";
 import { Application } from "express";
 // import { helmet } from "../../modules";
-import cors from 'cors';
-import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import * as OpenApiValidator from 'express-openapi-validator';
-import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
-import mongoose from 'mongoose';
-import uberCloneApiRoutes from '../routes';
-import config from './env'
+import cors from "cors";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import * as OpenApiValidator from "express-openapi-validator";
+import { OpenAPIV3 } from "express-openapi-validator/dist/framework/types";
+import mongoose from "mongoose";
+import uberCloneApiRoutes from "../routes";
+import config from "./env";
 import apiMessages from "../../utils/contants/api.messages";
+// Passport necessities
+import session from "express-session";
+import passport from "passport";
+import Strategies from "./strategies";
 
 const dbOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  };
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
 
 const docOptions = {
-    definition: {
-        openapi: "3.0.1",
-        info: {
-            title: "Uber Clone API",
-            version: "1.0.0",
-            description: "Uber Clone APIs",
-        },
-        servers: [
-            {
-                url: "http://localhost:8000",
-            },
-        ],
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Uber Clone API",
+      version: "1.0.0",
+      description: "Uber Clone APIs",
     },
-    apis: ["../routes/*.ts"],
-}
-
+    servers: [
+      {
+        url: "http://localhost:8000",
+      },
+    ],
+  },
+  apis: ["../routes/*.ts"],
+};
 
 const specs = swaggerJSDoc(docOptions);
 class App {
-    private _port: String = config.PORT ?? '6789';
-    private _app: Application;
-    private _logger: any;
+  private _port: String = config.PORT ?? "6789";
+  private _app: Application;
+  private _logger: any;
 
-    constructor(app: Application,logger: any) {
-        this._app = app;
-        this._logger = logger;
-    }
+  constructor(app: Application, logger: any) {
+    this._app = app;
+    this._logger = logger;
+  }
 
-    public async init (): Promise<void> {
-        this.setupExpress();
-        await this.docsSetup();
-        await this.run();
+  public async init(): Promise<void> {
+    this.setupExpress();
+    await this.setupPassport();
+    await this.docsSetup();
+    await this.run();
+  }
 
-    }
+  private setupPassport(): void {
+    Strategies.config();
+  }
 
-    private setupExpress(): void {
-        this._app.use(json());
-        this._app.use(cors());
-        // this._app.use(helmet());
-        this._app.use(morgan('combined', {}));
-        this._app.get('/', (_, res) => res.json({message: apiMessages.WELCOME_MESSAGE}));
-        this._app.use(apiMessages.V1, uberCloneApiRoutes);
-        this._app.use((_, __, next) => {
-            next(apiMessages.NOT_FOUND_API)
-        })
+  private setupExpress(): void {
+    this._app.use(
+      session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: true },
+      })
+    );
+    this._app.use(passport.initialize());
+    this._app.use(passport.session());
 
-    }
+    this._app.use(json());
+    this._app.use(cors());
+    // this._app.use(helmet());
+    this._app.use(morgan("combined", {}));
+    this._app.get("/", (_, res) =>
+      res.json({ message: apiMessages.WELCOME_MESSAGE })
+    );
+    this._app.use(apiMessages.V1, uberCloneApiRoutes);
+    this._app.use((_, __, next) => {
+      next(apiMessages.NOT_FOUND_API);
+    });
+  }
 
   private async docsSetup(): Promise<void> {
-    this._app.use(`${apiMessages.V1}/docs`, swaggerUi.serve, swaggerUi.setup(specs));
+    this._app.use(
+      `${apiMessages.V1}/docs`,
+      swaggerUi.serve,
+      swaggerUi.setup(specs)
+    );
     this._app.use(
       OpenApiValidator.middleware({
         apiSpec: specs as OpenAPIV3.Document,
@@ -79,21 +102,29 @@ class App {
     );
   }
 
-    private setupControllers(): void {
-        // this.addControllers([])
-    }
+  private setupControllers(): void {
+    // this.addControllers([])
+  }
 
-    private async run (): Promise<void> {
-        mongoose.connect(config.DATABASE_URL ?? process.env.UBER_CLONE_DB ?? '', dbOptions).then(() => {
-            this._logger.info('🚀 Database connected');
-            this._app.listen(this._port, () => {
-                this._logger.info(`🚀🚀 Server ready at http://localhost:${this._port}${apiMessages.V1}`);
-            });
-        }).catch((error: any) => {
-            this._logger.error(error.message);
-            process.exit(1);
-        })
-    }
+  private async run(): Promise<void> {
+    mongoose
+      .connect(
+        config.DATABASE_URL ?? process.env.UBER_CLONE_DB ?? "",
+        dbOptions
+      )
+      .then(() => {
+        this._logger.info("🚀 Database connected");
+        this._app.listen(this._port, () => {
+          this._logger.info(
+            `🚀🚀 Server ready at http://localhost:${this._port}${apiMessages.V1}`
+          );
+        });
+      })
+      .catch((error: any) => {
+        this._logger.error(error.message);
+        process.exit(1);
+      });
+  }
 }
 
 export default App;
--- app/routes/v1/auth.ts
+++ app/routes/v1/auth.ts
@@ -1,11 +1,26 @@
-import { Router } from 'express';
+import { Router } from "express";
+import passport from "passport";
 
 const router = Router();
 
-router.post('/login',  (re, res) => {
-    res.json({
-        message: 'Logged in'
-    })
+router.post("/login", (re, res) => {
+  res.json({
+    message: "Logged in",
+  });
 });
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
 
 export default router;
